59625ec46929ce4d00453a95eb823568d7565c14
Juan Dorado <jfdoradotr@icloud.com>
2023-12-23 12:47:55 -0600

    Initial Commit

---
+++ Lifehacks/LifehacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifehacksApp.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LifehacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
