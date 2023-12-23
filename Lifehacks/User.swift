0cafd169336779943b0da00d8010ef718021d1bf
Juan Dorado <jfdoradotr@icloud.com>
2023-12-23 12:50:07 -0600

    Create the basic models, `User` and `Question`

---
+++ Lifehacks/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 23/12/23.
+//
+
+import Foundation
+
+struct User {
+  let id: Int
+  let reputation: Int
+  let name: String
+  let aboutMe: String?
+  let profileImageURL: URL?
+}
