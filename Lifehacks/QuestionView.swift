9ee61489f771159fe4d45cd1af59ffcc8ca51c6c
Juan Dorado <jfdoradotr@icloud.com>
2023-12-24 11:44:43 -0600

    Create the demo data to use on previews

---
+++ Lifehacks/QuestionView.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionView.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 24/12/23.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text("In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew.")
+        .font(.headline)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
