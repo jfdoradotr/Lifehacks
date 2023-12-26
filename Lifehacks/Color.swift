a04100fdf917b25c04b158159b8e5b75746880ea
Juan Dorado <jfdoradotr@icloud.com>
2023-12-26 12:15:30 -0600

    Add a color preview for the new resources

---
+++ Lifehacks/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 25/12/23.
+//
+
+import SwiftUI
+
+struct Color_Previews: PreviewProvider {
+  static var previews: some View {
+    VStack {
+      Color.accentColor
+      Color.pizazz
+      Color.electricViolet
+      Color.blazeOrange
+    }
+  }
+}
