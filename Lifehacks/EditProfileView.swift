bfef4377a0ae43e3766473d01b6ce75cf0dcf24b
Juan Dorado <jfdoradotr@icloud.com>
2024-01-13 17:55:58 -0600

    Create an empty EditProfile view class

---
+++ Lifehacks/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 13/01/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  EditProfileView()
+}
