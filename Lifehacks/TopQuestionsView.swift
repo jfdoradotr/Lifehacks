23acade8d6e6e53af4fd439905e014caa22558a3
Juan Dorado <jfdoradotr@icloud.com>
2024-01-14 11:21:16 -0600

    Fix indentation

--- Lifehacks/TopQuestionsView.swift
+++ Lifehacks/TopQuestionsView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct TopQuestionsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 // MARK: - Row
