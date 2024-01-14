bc3ec9db4cc7c631be3b2bc475bfe8ccc78ff4ce
Juan Dorado <jfdoradotr@icloud.com>
2024-01-14 11:11:50 -0600

    Create the Counter view at a Row level

--- Lifehacks/TopQuestionsView.swift
+++ Lifehacks/TopQuestionsView.swift
@@ -13,6 +13,38 @@ struct TopQuestionsView: View {
     }
 }
 
+// MARK: - Row
+
+extension TopQuestionsView {
+  struct Row: View {
+    var body: some View {
+      Text("Hello, World!")
+    }
+  }
+}
+
+// MARK: - Counter
+
+extension TopQuestionsView.Row {
+  struct Counter: View {
+    let count: Int
+    let label: String
+
+    var body: some View {
+      VStack {
+        Text("\(count)")
+          .font(.title3)
+          .bold()
+        Text(label)
+          .font(.caption)
+      }
+      .frame(width: 67, height: 67)
+    }
+  }
+}
+
+// MARK: - Previews
+
 #Preview {
     TopQuestionsView()
 }
