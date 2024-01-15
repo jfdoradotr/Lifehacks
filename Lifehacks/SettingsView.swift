d04e21713dfad0e567aeaa9c7c149b18a75986ab
Juan Dorado <jfdoradotr@icloud.com>
2024-01-14 19:31:19 -0600

    Create the subviews that will be used for the SettingsView

--- Lifehacks/SettingsView.swift
+++ Lifehacks/SettingsView.swift
@@ -13,6 +13,42 @@ struct SettingsView: View {
   }
 }
 
+// MARK: - Row
+
+extension SettingsView {
+  struct Row: View {
+    let theme: Theme
+
+    var body: some View {
+      LabeledContent {
+        Placeholder(imageName: "sun.max.fill")
+          .style(color: theme.accentColor)
+        Placeholder(imageName: "moon.fill")
+          .style(color: theme.secondaryColor)
+        Placeholder(imageName: "leaf")
+          .style(color: theme.secondaryColor, isFilled: false)
+      } label: {
+        Text(theme.name)
+      }
+      .font(.headline)
+      .foregroundColor(.primary)
+    }
+  }
+}
+
+extension SettingsView.Row {
+  struct Placeholder: View {
+    let imageName: String
+
+    var body: some View {
+      Image(systemName: imageName)
+        .frame(width: 42.0, height: 42.0)
+    }
+  }
+}
+
+// MARK: - Previews
+
 #Preview {
   SettingsView()
 }
