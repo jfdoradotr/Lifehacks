4fa46e32481a9d84bdb442082e48015194a6f24a
Juan Dorado <jfdoradotr@icloud.com>
2024-01-14 19:28:21 -0600

    Create the SettingsView file

---
+++ Lifehacks/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 14/01/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  SettingsView()
+}
