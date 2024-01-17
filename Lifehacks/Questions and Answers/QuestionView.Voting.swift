45752bddc45e881cd599b1d023a5e1c28e6d4b86
Juan Dorado <jfdoradotr@icloud.com>
2024-01-17 13:21:50 -0600

    Use dynamicTypeSize environment to check and rearrange the views to fit on the small space horizontally

--- Lifehacks/Questions and Answers/QuestionView.Voting.swift
+++ Lifehacks/Questions and Answers/QuestionView.Voting.swift
@@ -17,13 +17,18 @@ extension QuestionView {
     let downvote: () -> Void
     let unvote: () -> Void
 
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
+
     enum Vote {
       case up, down
     }
 
     var body: some View {
-      VStack(spacing: 8.0) {
-        content
+      if dynamicTypeSize.isAccessibilitySize {
+        HStack { content }
+      } else {
+        VStack(spacing: 8.0) { content }
+          .frame(minWidth: 56.0)
       }
     }
 
