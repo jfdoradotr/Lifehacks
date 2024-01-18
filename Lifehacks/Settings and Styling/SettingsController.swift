3d9ddaa4805f8d4ad29c742d40df3fb05f351f77
Juan Dorado <jfdoradotr@icloud.com>
2024-01-18 16:38:12 -0600

    Create the Settings controller where we will handle the settings behavior

---
+++ Lifehacks/Settings and Styling/SettingsController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsController.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 18/01/24.
+//
+
+import Foundation
+
+final class SettingsController: ObservableObject {
+  @Published var selectedThemeID: String? = Theme.default.id
+
+  var theme: Theme {
+    guard let selectedThemeID, let theme = Theme.allThemes.first(where: { $0.id == selectedThemeID }) else {
+      return Theme.default
+    }
+    return theme
+  }
+}
