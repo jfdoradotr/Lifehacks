ba6a33b370d7771d73c330a6de576daad3e2def1
Juan Dorado <jfdoradotr@icloud.com>
2024-01-19 17:01:03 -0600

    Using subscript locates the corresponding index in the questions array

--- Lifehacks/Questions and Answers/QuestionsController.swift
+++ Lifehacks/Questions and Answers/QuestionsController.swift
@@ -8,12 +8,25 @@
 import Foundation
 
 final class QuestionsController: ObservableObject {
-  @Published var questions: [Question]
+  @Published var questions: [Question] {
+    didSet { persistenceController.save(questions: questions) }
+  }
 
   private let persistenceController: PersistenceController
+  
+  subscript(id: Int) -> Question {
+    get { questions[index(for: id)] }
+    set { questions[index(for: id)] = newValue }
+  }
 
   init(persistenceController: PersistenceController) {
     self.questions = persistenceController.fetchQuestions() ?? []
     self.persistenceController = persistenceController
+  }
+}
+
+private extension QuestionsController {
+  func index(for id: Int) -> Int {
+    questions.firstIndex(where: { $0.id == id })!
   }
 }
