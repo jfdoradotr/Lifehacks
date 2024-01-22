0a5019e39fe9f4fd4b5241f71d7666d0d9dc6e59
Juan Dorado <jfdoradotr@icloud.com>
2024-01-22 17:19:13 -0600

    Create a View+Loading extension to handle the loading and error states

---
+++ Lifehacks/View+Loading.swift
@@ -0,0 +1,26 @@
+//
+//  View+Loading.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 22/01/24.
+//
+
+import SwiftUI
+
+extension View {
+  func loading(_ isLoading: Bool) -> some View {
+    overlay {
+      if isLoading {
+        ProgressView("Loading...")
+          .padding()
+          .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 8.0))
+      }
+    }
+  }
+
+  func errorAlert(isPresented: Binding<Bool>) -> some View {
+    alert(isPresented: isPresented) {
+      Alert(title: Text("There was an error"), message: Text("Please try again later"))
+    }
+  }
+}
