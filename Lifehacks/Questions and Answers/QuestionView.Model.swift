761dcb44e529f27fb317606a77d931cbffe04e13
Juan Dorado <jfdoradotr@icloud.com>
2024-01-22 17:26:17 -0600

    Create the view model to download the details of question

---
+++ Lifehacks/Questions and Answers/QuestionView.Model.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionView.Model.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 22/01/24.
+//
+
+import Foundation
+
+extension QuestionView {
+  @MainActor class Model: ObservableObject {
+    @Published var question: Question
+    @Published var isLoading = false
+    @Published var showError = false
+
+    init(question: Question) {
+      self.question = question
+    }
+
+    func loadDetails() async {
+      isLoading = true
+      defer { isLoading = false }
+      let url = URL.apiRequestURL(path: "/questions/\(question.id)", parameters: [
+        "filter": "-T2M9.3eG9hXfgHaShP_t9UiIkcZIWFCzoC_"
+      ])
+      do {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let wrapper = try JSONDecoder.apiDecoder.decode(Question.Wrapper.self, from: data)
+        question = wrapper.items[0]
+      } catch {
+        showError = true
+      }
+    }
+  }
+}
